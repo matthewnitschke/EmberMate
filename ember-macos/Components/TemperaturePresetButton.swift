62c8ed6509c9c9106fab0a59a1ec267b0f699b77
Matthew Nitschke <matthewcnitschke@gmail.com>
2023-12-28 16:45:24 -0700

    Fixed issue where presets always displayed as celsius

--- ember-macos/Components/TemperaturePresetButton.swift
+++ ember-macos/Components/TemperaturePresetButton.swift
@@ -10,13 +10,14 @@ import SwiftUI
 
 struct TemperaturePresetButton: View {
     var preset: Preset
+    var temperatureUnit: TemperatureUnit
     var isSelected: Bool
     var onSelect: (Double) -> Void
     
     var body: some View {
         IconButton(
             headerText: preset.name,
-            footerText: getFormattedTemperature(preset.temperature, unit: .celcius),
+            footerText: getFormattedTemperature(preset.temperature, unit: temperatureUnit),
             icon: preset.icon,
             isSelected: isSelected,
             onSelect: {
